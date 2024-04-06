9ecedd5ea19a5f97ca239aa97cecc8a229090957
zqw1299x <1299133697@qq.com>
2024-04-06 21:33:08 +0800

    修改工程名为USART-RX_TX

---
+++ USART—RX_TX/Output/healthmanage.d
@@ -0,0 +1,4 @@
+..\..\output\healthmanage.o: ..\..\User\healthManage.c
+..\..\output\healthmanage.o: ..\..\User\healthManage.h
+..\..\output\healthmanage.o: D:\BaiduNetdiskDownload\SoftwareAZB\ARM\ARMCC\Bin\..\include\stdint.h
+..\..\output\healthmanage.o: D:\BaiduNetdiskDownload\SoftwareAZB\ARM\ARMCC\Bin\..\include\string.h

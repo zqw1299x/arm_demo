856158cbb743cb43612bd957f2f88d77cdb7d749
zqw1299x <115332729+zqw1299x@users.noreply.github.com>
2024-04-06 22:10:38 +0800

    Delete USART—USART1_RX_TX directory

--- USART—USART1_RX_TX/Output/healthmanage.d
+++
@@ -1,4 +0,0 @@
-..\..\output\healthmanage.o: ..\..\User\healthManage.c
-..\..\output\healthmanage.o: ..\..\User\healthManage.h
-..\..\output\healthmanage.o: D:\BaiduNetdiskDownload\SoftwareAZB\ARM\ARMCC\Bin\..\include\stdint.h
-..\..\output\healthmanage.o: D:\BaiduNetdiskDownload\SoftwareAZB\ARM\ARMCC\Bin\..\include\string.h
